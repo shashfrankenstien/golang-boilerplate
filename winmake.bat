4e1156936e0b248f9d9d8326045e2696d0943680
Gopikrishna <S.Gopikrishna@thomsonreuters.com>
2020-02-07 01:05:15 -0500

    First commit

---
+++ winmake.bat
@@ -0,0 +1,38 @@
+@echo off
+echo %1
+
+set make=%~dpnx0
+set EXE=AppName.exe
+
+if [%1]==[build] goto :BUILD
+if [%1]==[install] goto :INSTALL
+if [%1]==[uninstall] goto :UNINSTALL
+if [%1]==[clean] goto :CLEAN
+goto :ALL
+goto :DONE
+
+:ALL
+	goto :BUILD
+goto :DONE
+
+:BUILD
+	mkdir build
+	cd src
+	go build -o ..\build\%EXE%
+	cd ..
+goto :DONE
+
+:CLEAN
+	rm -rf build
+goto :DONE
+
+:INSTALL
+	%make% build && cp build\%EXE% %GOROOT%\bin && %make% clean
+goto :DONE
+
+:UNINSTALL
+	rm %GOROOT%\bin\%EXE%
+goto :DONE
+
+:DONE
+	echo done.
